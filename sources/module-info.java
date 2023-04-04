32d5d5dccc69caa87617be1627bb76f7a84c8872
Pierre Convert <pierreconvert@macbook-pro-6.lan>
2023-04-04 20:00:11 +0200

    new build workflow applied

--- sources/module-info.java
+++ sources/module-info.java
@@ -27,13 +27,16 @@ module com.s8.web.carbon {
 	/* </carbon> */
 
 	
-	
+	/* <dependencies> */
 	requires transitive com.s8.io.bytes;
 	requires transitive com.s8.io.xml;
 	requires transitive com.s8.io.csv;
 	
 	requires transitive com.s8.arch.silicon;
-	requires transitive com.s8.web.helium;
 	requires transitive com.s8.arch.magnesium;
+	
+	requires transitive com.s8.web.helium;
+
+	/* </dependencies> */
 	
 }
